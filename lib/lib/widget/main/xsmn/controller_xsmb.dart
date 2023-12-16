ddd03cdc28987bb29a0ad1dc5edd2cb6968b4e00
roy <roy93group@gmail.com>
2023-12-16 20:34:39 +0700

    update

--- lib/lib/widget/main/xsmn/controller_xsmb.dart
+++ lib/lib/widget/main/xsmn/controller_xsmb.dart
@@ -1,15 +1,76 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:ketquaxoso/lib/common/const/string_constants.dart';
 import 'package:ketquaxoso/lib/core/base_controller.dart';
 import 'package:ketquaxoso/lib/util/log_dog_utils.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class ControllerXSMN extends BaseController {
   var selectedDateTime = DateTime.now().obs;
+  var webViewController = WebViewController().obs;
 
   void clearOnDispose() {
     Get.delete<ControllerXSMN>();
   }
 
   void setSelectedDateTime(DateTime dateTime) {
+    debugPrint("roy93~ setSelectedDateTime $dateTime");
     selectedDateTime.value = dateTime;
+
+    var day = "";
+    if (dateTime.day >= 10) {
+      day = "${dateTime.day}";
+    } else {
+      day = "0${dateTime.day}";
+    }
+    var month = "";
+    if (dateTime.month >= 10) {
+      month = "${dateTime.month}";
+    } else {
+      month = "0${dateTime.month}";
+    }
+    var date = "#n$day-$month-${dateTime.year}";
+    // debugPrint("date $date");
+    var link = "${StringConstants.kqMienNam}$date";
+    debugPrint("roy93~ link $link");
+
+    webViewController.value = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(Colors.white)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // debugPrint("progress $progress");
+          },
+          onPageStarted: (String url) {
+            // debugPrint("onPageStarted url $url");
+          },
+          onPageFinished: (String url) async {
+            // debugPrint("onPageFinished url $url");
+            addBottomSpace();
+          },
+          onWebResourceError: (WebResourceError error) {
+            // debugPrint("onPageFinished url $error");
+          },
+          onNavigationRequest: (NavigationRequest request) {
+            // debugPrint("request ${request.url}");
+            if (request.url.contains(".html")) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(link));
+  }
+
+  Future<void> addBottomSpace() async {
+    const script = '''
+      var spaceDiv = document.createElement("div");
+      spaceDiv.style.height = "150px";
+      document.body.appendChild(spaceDiv);
+    ''';
+
+    await webViewController.value.runJavaScript(script);
   }
 }
--- lib/lib/widget/main/xsmn/xsmn_screen.dart
+++ lib/lib/widget/main/xsmn/xsmn_screen.dart
@@ -1,9 +1,9 @@
 import 'package:calendar_timeline_sbk/calendar_timeline.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:ketquaxoso/lib/common/const/color_constants.dart';
-import 'package:ketquaxoso/lib/common/const/string_constants.dart';
 import 'package:ketquaxoso/lib/core/base_stateful_state.dart';
-import 'package:ketquaxoso/lib/util/duration_util.dart';
+import 'package:ketquaxoso/lib/widget/main/xsmn/controller_xsmb.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class XSMNScreen extends StatefulWidget {
@@ -16,40 +16,12 @@ class XSMNScreen extends StatefulWidget {
 }
 
 class _XSMNScreenState extends BaseStatefulState<XSMNScreen> {
-  var _wvController = WebViewController()
-    ..setJavaScriptMode(JavaScriptMode.unrestricted)
-    ..setBackgroundColor(Colors.white);
-
-  // final ControllerXSMN _controllerXSMN = Get.put(ControllerXSMN());
+  final ControllerXSMN _controllerXSMN = Get.put(ControllerXSMN());
 
   @override
   void initState() {
     super.initState();
-    _wvController.setNavigationDelegate(
-      NavigationDelegate(
-        onProgress: (int progress) {
-          // debugPrint("roy93~ progress $progress");
-        },
-        onPageStarted: (String url) {
-          debugPrint("roy93~ onPageStarted url $url");
-        },
-        onPageFinished: (String url) async {
-          debugPrint("roy93~ onPageFinished url $url");
-          addBottomSpace();
-        },
-        onWebResourceError: (WebResourceError error) {
-          debugPrint("roy93~ onPageFinished url $error");
-        },
-        onNavigationRequest: (NavigationRequest request) {
-          debugPrint("roy93~ request ${request.url}");
-          if (request.url.contains(".html")) {
-            return NavigationDecision.prevent;
-          }
-          return NavigationDecision.navigate;
-        },
-      ),
-    );
-    _selectDay(DateTime.now(), true);
+    _selectDay(DateTime.now());
   }
 
   @override
@@ -85,11 +57,11 @@ class _XSMNScreenState extends BaseStatefulState<XSMNScreen> {
       width: double.infinity,
       padding: const EdgeInsets.fromLTRB(0, 8, 16, 0),
       child: CalendarTimeline(
-        initialDate: DateTime.now(),
+        initialDate: _controllerXSMN.selectedDateTime.value,
         firstDate: DateTime.now().subtract(const Duration(days: 365)),
         lastDate: DateTime.now().add(const Duration(days: 365)),
         onDateSelected: (date) {
-          _selectDay(date, false);
+          _selectDay(date);
         },
         leftMargin: 0,
         monthColor: Colors.black,
@@ -104,66 +76,12 @@ class _XSMNScreenState extends BaseStatefulState<XSMNScreen> {
   }
 
   Widget _buildWebView() {
-    return WebViewWidget(controller: _wvController);
+    return Obx(() {
+      return WebViewWidget(controller: _controllerXSMN.webViewController.value);
+    });
   }
 
-  Future<void> addBottomSpace() async {
-    const script = '''
-      var spaceDiv = document.createElement("div");
-      spaceDiv.style.height = "150px";
-      document.body.appendChild(spaceDiv);
-    ''';
-
-    await _wvController.runJavaScript(script);
-  }
-
-  void _selectDay(DateTime dateTime, bool isFirstInit) {
-    // debugPrint("dateTime $dateTime");
-    var day = "";
-    if (dateTime.day >= 10) {
-      day = "${dateTime.day}";
-    } else {
-      day = "0${dateTime.day}";
-    }
-    var date = "#n$day-${dateTime.month}-${dateTime.year}";
-    // debugPrint("date $date");
-    var link = "${StringConstants.kqMienNam}$date";
-    debugPrint("roy93~ link $link");
-
-    if (isFirstInit) {
-      _wvController.loadRequest(Uri.parse(link));
-    } else {
-      _wvController = WebViewController()
-        ..setJavaScriptMode(JavaScriptMode.unrestricted)
-        ..setBackgroundColor(Colors.white);
-      _wvController.setNavigationDelegate(
-        NavigationDelegate(
-          onProgress: (int progress) {
-            // debugPrint("roy93~ progress $progress");
-          },
-          onPageStarted: (String url) {
-            debugPrint("roy93~ onPageStarted url $url");
-          },
-          onPageFinished: (String url) async {
-            debugPrint("roy93~ onPageFinished url $url");
-            addBottomSpace();
-          },
-          onWebResourceError: (WebResourceError error) {
-            debugPrint("roy93~ onPageFinished url $error");
-          },
-          onNavigationRequest: (NavigationRequest request) {
-            debugPrint("roy93~ request ${request.url}");
-            if (request.url.contains(".html")) {
-              return NavigationDecision.prevent;
-            }
-            return NavigationDecision.navigate;
-          },
-        ),
-      );
-      _wvController.loadRequest(Uri.parse(link));
-      setState(() {
-
-      });
-    }
+  void _selectDay(DateTime dateTime) {
+    _controllerXSMN.setSelectedDateTime(dateTime);
   }
 }
