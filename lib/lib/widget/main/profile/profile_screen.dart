2765f7829134c21c489823189cfb511e60fbd9d7
roy <roy93group@gmail.com>
2023-12-16 23:47:01 +0700

    update

--- lib/lib/widget/main/profile/profile_screen.dart
+++ lib/lib/widget/main/profile/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
 import 'package:ketquaxoso/lib/common/const/color_constants.dart';
 import 'package:ketquaxoso/lib/core/base_stateful_state.dart';
@@ -28,6 +29,32 @@ class _ProfileScreenState extends BaseStatefulState<ProfileScreen> {
       body: Container(
         alignment: Alignment.center,
         color: ColorConstants.bkg,
+        child: Stack(
+          children: [
+            Image.asset(
+              "assets/images/bkg_3.jpg",
+              height: double.infinity,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            ListView(
+              physics: const BouncingScrollPhysics(),
+              padding: const EdgeInsets.fromLTRB(16, 48, 16, 16),
+              children: [
+                AvatarGlow(
+                  glowColor: Colors.white,
+                  endRadius: 60.0,
+                  showTwoGlows: true,
+                  child: Image.asset(
+                    'assets/images/anim_2.gif',
+                    width: 90,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
