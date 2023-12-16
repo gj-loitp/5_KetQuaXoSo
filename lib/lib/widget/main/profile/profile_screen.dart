671873bd62c0693d6bf021e3cf9c4fd55a2e8791
roy <roy93group@gmail.com>
2023-12-17 00:02:13 +0700

    update

--- lib/lib/widget/main/profile/profile_screen.dart
+++ lib/lib/widget/main/profile/profile_screen.dart
@@ -1,7 +1,9 @@
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:ketquaxoso/lib/common/const/color_constants.dart';
 import 'package:ketquaxoso/lib/core/base_stateful_state.dart';
+import 'package:toggle_switch/toggle_switch.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({
@@ -49,6 +51,49 @@ class _ProfileScreenState extends BaseStatefulState<ProfileScreen> {
                     'assets/images/anim_2.gif',
                     width: 90,
                     fit: BoxFit.cover,
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 16),
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(16),
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(45.0)),
+                    color: Colors.white,
+                  ),
+                  child: Column(
+                    children: [
+                      const Text(
+                        "Chọn giao diện",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 22,
+                          color: Colors.black,
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      ToggleSwitch(
+                        minWidth: Get.width / 3,
+                        initialLabelIndex: 1,
+                        cornerRadius: 45.0,
+                        activeFgColor: Colors.white,
+                        inactiveBgColor: Colors.grey,
+                        inactiveFgColor: Colors.white,
+                        totalSwitches: 2,
+                        labels: const ['Webview', 'Native view'],
+                        icons: const [
+                          Icons.looks_one,
+                          Icons.looks_two,
+                        ],
+                        activeBgColors: const [
+                          [ColorConstants.appColor],
+                          [ColorConstants.appColor]
+                        ],
+                        onToggle: (index) {
+                          debugPrint('switched to: $index');
+                        },
+                      ),
+                    ],
                   ),
                 ),
               ],
