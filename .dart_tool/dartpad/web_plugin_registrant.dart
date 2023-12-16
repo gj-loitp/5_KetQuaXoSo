74dc7d77233dcca2d453ae6f8ed38d84f8c4cb34
roy <roy93group@gmail.com>
2023-12-16 12:15:38 +0700

    update

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -8,10 +8,7 @@
 
 import 'package:connectivity_plus/src/connectivity_plus_web.dart';
 import 'package:device_info_plus/src/device_info_plus_web.dart';
-import 'package:file_picker/_internal/file_picker_web.dart';
 import 'package:flutter_native_splash/flutter_native_splash_web.dart';
-import 'package:image_picker_for_web/image_picker_for_web.dart';
-import 'package:package_info_plus/src/package_info_plus_web.dart';
 import 'package:platform_device_id_web/platform_device_id_web.dart';
 import 'package:share_plus/src/share_plus_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
@@ -22,10 +19,7 @@ void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
   ConnectivityPlusWebPlugin.registerWith(registrar);
   DeviceInfoPlusWebPlugin.registerWith(registrar);
-  FilePickerWeb.registerWith(registrar);
   FlutterNativeSplashWeb.registerWith(registrar);
-  ImagePickerPlugin.registerWith(registrar);
-  PackageInfoPlusWebPlugin.registerWith(registrar);
   PlatformDeviceIdWebPlugin.registerWith(registrar);
   SharePlusWebPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,7 @@ import Foundation
 
 import connectivity_plus
 import device_info_plus
-import file_selector_macos
-import flutter_local_notifications
 import in_app_review
-import package_info_plus
 import path_provider_foundation
 import platform_device_id
 import platform_device_id_macos
@@ -22,10 +19,7 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
-  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   InAppReviewPlugin.register(with: registry.registrar(forPlugin: "InAppReviewPlugin"))
-  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PlatformDeviceIdMacosPlugin.register(with: registry.registrar(forPlugin: "PlatformDeviceIdMacosPlugin"))
   PlatformDeviceIdMacosPlugin.register(with: registry.registrar(forPlugin: "PlatformDeviceIdMacosPlugin"))
