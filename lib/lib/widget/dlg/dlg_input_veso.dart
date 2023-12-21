809113bd52c912174783e70083146ee61b89384d
roy <roy93group@gmail.com>
2023-12-21 22:02:20 +0700

    update

--- lib/lib/widget/dlg/dlg_input_veso.dart
+++ lib/lib/widget/dlg/dlg_input_veso.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 class DlgInputVeSo extends StatefulWidget {
@@ -22,6 +23,7 @@ class _DlgInputVeSoState extends State<DlgInputVeSo> {
           color: Colors.white,
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
               alignment: Alignment.centerRight,
@@ -29,13 +31,50 @@ class _DlgInputVeSoState extends State<DlgInputVeSo> {
                 child: InkWell(
                   customBorder: const CircleBorder(),
                   child: const Icon(
-                    Icons.clear,
+                    Icons.cancel,
                     color: Colors.black,
-                    size: 48.0,
+                    size: 32.0,
                   ),
                   onTap: () {
                     Get.back();
                   },
+                ),
+              ),
+            ),
+            const Text(
+              "Mời nhập thông tin",
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                color: Colors.black,
+                fontSize: 22,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 16),
+              height: 52,
+              alignment: Alignment.centerRight,
+              child: TextField(
+                textInputAction: TextInputAction.next,
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly,
+                ],
+                maxLength: 6,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.local_atm),
+                  hintText: "Nhập đúng dãy số của bạn",
+                  hintStyle: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    color: Colors.grey,
+                    fontSize: 18,
+                  ),
+                  counterText: "",
+                  border: OutlineInputBorder(),
+                ),
+                style: const TextStyle(
+                  fontWeight: FontWeight.w700,
+                  color: Colors.black,
+                  fontSize: 22,
                 ),
               ),
             ),
