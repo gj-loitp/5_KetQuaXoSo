ef69919ecf062d180f8b5729b7458264ee0b298a
roy <roy93group@gmail.com>
2023-12-21 22:13:14 +0700

    update

--- lib/lib/widget/dlg/dlg_input_veso.dart
+++ lib/lib/widget/dlg/dlg_input_veso.dart
@@ -10,6 +10,8 @@ class DlgInputVeSo extends StatefulWidget {
 }
 
 class _DlgInputVeSoState extends State<DlgInputVeSo> {
+  final TextEditingController _dateController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Dialog(
@@ -83,12 +85,12 @@ class _DlgInputVeSoState extends State<DlgInputVeSo> {
               height: 52,
               alignment: Alignment.centerRight,
               child: TextField(
+                controller: _dateController,
                 textInputAction: TextInputAction.next,
                 inputFormatters: [
-                  FilteringTextInputFormatter.digitsOnly,
+                  DateTextFormatter(),
                 ],
-                maxLength: 6,
-                keyboardType: TextInputType.number,
+                keyboardType: TextInputType.datetime,
                 decoration: const InputDecoration(
                   icon: Icon(Icons.date_range),
                   hintText: "dd/MM/yyyy",
@@ -111,5 +113,40 @@ class _DlgInputVeSoState extends State<DlgInputVeSo> {
         ),
       ),
     );
+  }
+}
+
+class DateTextFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
+    if (newValue.text.length > oldValue.text.length && newValue.text.isNotEmpty && oldValue.text.isNotEmpty) {
+      if (RegExp('[^0-9/]').hasMatch(newValue.text)) return oldValue;
+      if (newValue.text.length > 10) return oldValue;
+      if (newValue.text.length == 2 || newValue.text.length == 5) {
+        return TextEditingValue(
+          text: '${newValue.text}/',
+          selection: TextSelection.collapsed(
+            offset: newValue.selection.end + 1,
+          ),
+        );
+      } else if (newValue.text.length == 3 && newValue.text[2] != '/') {
+        return TextEditingValue(
+          text: '${newValue.text.substring(0, 2)}/${newValue.text.substring(2)}',
+          selection: TextSelection.collapsed(
+            offset: newValue.selection.end + 1,
+          ),
+        );
+      } else if (newValue.text.length == 6 && newValue.text[5] != '/') {
+        return TextEditingValue(
+          text: '${newValue.text.substring(0, 5)}/${newValue.text.substring(5)}',
+          selection: TextSelection.collapsed(
+            offset: newValue.selection.end + 1,
+          ),
+        );
+      }
+    } else if (newValue.text.length == 1 && oldValue.text.isEmpty && RegExp('[^0-9]').hasMatch(newValue.text)) {
+      return oldValue;
+    }
+    return newValue;
   }
 }
