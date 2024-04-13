3d2eee040088ccb9919a4698b7b094ee685a0092
roy <roy93group@gmail.com>
2024-04-13 22:30:05 +0700

    update

--- lib/lib/widget/introduction/introduction_screen.dart
+++ lib/lib/widget/introduction/introduction_screen.dart
@@ -1,9 +1,9 @@
-import 'package:blur/blur.dart';
+import 'package:animated_introduction/animated_introduction.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:ketquaxoso/lib/common/const/color_constants.dart';
 import 'package:ketquaxoso/lib/core/base_stateful_state.dart';
 
-//TODO roy93~
 class IntroductionScreen extends StatefulWidget {
   const IntroductionScreen({
     super.key,
@@ -14,6 +14,24 @@ class IntroductionScreen extends StatefulWidget {
 }
 
 class _IntroductionScreenState extends BaseStatefulState<IntroductionScreen> {
+  final List<SingleIntroScreen> pages = [
+    const SingleIntroScreen(
+      title: 'Welcome to the Event Management App !',
+      description: 'You plans your Events, We\'ll do the rest and will be the best! Guaranteed!  ',
+      imageAsset: 'assets/onboard_one.png',
+    ),
+    const SingleIntroScreen(
+      title: 'Book tickets to cricket matches and events',
+      description: 'Tickets to the latest movies, crickets matches, concerts, comedy shows, plus lots more !',
+      imageAsset: 'assets/onboard_two.png',
+    ),
+    const SingleIntroScreen(
+      title: 'Grabs all events now only in your hands',
+      description: 'All events are now in your hands, just a click away ! ',
+      imageAsset: 'assets/onboard_three.png',
+    ),
+  ];
+
   @override
   void initState() {
     super.initState();
@@ -27,24 +45,31 @@ class _IntroductionScreenState extends BaseStatefulState<IntroductionScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        color: ColorConstants.bkg,
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Image.asset(
-              "assets/images/bkg_3.jpg",
-              height: double.infinity,
-              width: double.infinity,
-              fit: BoxFit.cover,
-            ).blurred(
-              colorOpacity: 0.0,
-              borderRadius: const BorderRadius.horizontal(right: Radius.circular(0)),
-              blur: 5,
-            ),
-          ],
-        ),
+      body: AnimatedIntroduction(
+        slides: pages,
+        containerBg: ColorConstants.appColor,
+        // activeDotColor: Colors.white,
+        // inactiveDotColor: Colors.grey,
+        // footerBgColor: Colors.white,
+        isFullScreen: false,
+        textColor: Colors.white,
+        footerGradients: const [
+          Colors.pink,
+          Colors.yellowAccent,
+          Colors.black,
+        ],
+        indicatorType: IndicatorType.diamond,
+        skipText: "Bỏ qua",
+        nextText: "Tiếp theo",
+        doneText: "Hoàn thành",
+        onSkip: () {
+          debugPrint("roy93~ onSkip");
+          Get.back();
+        },
+        onDone: () {
+          debugPrint("roy93~ onDone");
+          Get.back();
+        },
       ),
     );
   }
