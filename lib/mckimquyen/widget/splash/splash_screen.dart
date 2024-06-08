54f92ff9bed19120dfb14a96e895ed725a835566
roy <roy93group@gmail.com>
2024-06-08 20:38:41 +0700

    update

--- lib/mckimquyen/widget/splash/splash_screen.dart
+++ lib/mckimquyen/widget/splash/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:applovin_max/applovin_max.dart';
+import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:lunar/calendar/Lunar.dart';
@@ -26,19 +27,16 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends BaseStatefulState<SplashScreen> {
   final ControllerMain _controllerMain = Get.find();
-  Timer? _timer;
 
   void _initializeInterstitialAds() {
     AppLovinMAX.setInterstitialListener(InterstitialListener(
       onAdLoadedCallback: (ad) {
         // Interstitial ad is ready to be shown. AppLovinMAX.isInterstitialReady(_interstitial_ad_unit_id) will now return 'true'
         debugPrint('roy93~ Interstitial ad loaded from ${ad.networkName}');
-        _goToMainScreen();
       },
       onAdLoadFailedCallback: (adUnitId, error) {
         // Interstitial ad failed to load
         debugPrint('roy93~ Interstitial onAdLoadFailedCallback error $error');
-        _goToMainScreen();
       },
       onAdDisplayedCallback: (ad) {
         debugPrint("roy93~ Interstitial onAdDisplayedCallback");
@@ -81,16 +79,12 @@ class _SplashScreenState extends BaseStatefulState<SplashScreen> {
         debugPrint("roy93~ initializePlugin timeNow $timeNow");
         debugPrint("roy93~ initializePlugin duration ${timeNow - timeStartApp}");
         _initializeInterstitialAds();
-        _timer = Timer(const Duration(seconds: 10), () {
-          _goToMainScreen();
-        });
       }
     });
   }
 
   @override
   void dispose() {
-    _timer?.cancel();
     super.dispose();
   }
 
@@ -110,15 +104,34 @@ class _SplashScreenState extends BaseStatefulState<SplashScreen> {
               fit: BoxFit.cover,
             ),
             _buildClock(),
-            Container(
-              padding: const EdgeInsets.all(32),
-              width: 124,
-              height: 124,
-              child: const CircularProgressIndicator(
-                color: Colors.white,
-                strokeWidth: 6.0,
-                strokeCap: StrokeCap.round,
-              ),
+            Column(
+              children: [
+                const Spacer(),
+                AvatarGlow(
+                  glowColor: Colors.white,
+                  child: SizedBox(
+                    width: 180,
+                    height: 180,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(180, 180),
+                      ),
+                      onPressed: () {
+                        _goToMainScreen();
+                      },
+                      child: const Text(
+                        'Nhấn Vào\nTrang Chủ',
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 32),
+              ],
             ),
           ],
         ),
@@ -160,7 +173,6 @@ class _SplashScreenState extends BaseStatefulState<SplashScreen> {
   }
 
   Future<void> _goToMainScreen() async {
-    _timer?.cancel();
     var keyIsShowedIntroduction = await SharedPreferencesUtil.getBool(SharedPreferencesUtil.keyIsShowedIntroduction);
     if (keyIsShowedIntroduction == true) {
       Get.off(() => const MainScreen());
