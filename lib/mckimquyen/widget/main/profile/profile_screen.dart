691cdf47f46c97a7ac5a9af96325931aeba3e47a
roy <roy93group@gmail.com>
2024-06-08 18:36:11 +0700

    update

--- lib/mckimquyen/widget/main/profile/profile_screen.dart
+++ lib/mckimquyen/widget/main/profile/profile_screen.dart
@@ -316,91 +316,6 @@ class _ProfileScreenState extends BaseStatefulState<ProfileScreen> {
                               },
                             ),
                             const SizedBox(height: 32),
-                            //TODO roy93~ impl
-                            // UIUtils.getButton(
-                            //   "Lịch sử dò vé số",
-                            //   Icons.history,
-                            //   description: "Tra cứu lịch sử dò vé số của bạn 😤",
-                            //   () {
-                            //     Get.to(() => const HistoryScreen());
-                            //   },
-                            // ),
-                            UIUtils.getButton(
-                              "Tin tức",
-                              Icons.info,
-                              description: "Các thông tin hữu ích 😘",
-                              () {
-                                Get.to(() => const InformationScreen());
-                                _showInterAd();
-                              },
-                            ),
-                            //TODO roy93~ impl
-                            // UIUtils.getButton(
-                            //   "Xem tử vi trọn đời của 12 con giáp",
-                            //   Icons.people_sharp,
-                            //   description:
-                            //       "Tử vi trọn đời cung cấp cho bạn thông tin tổng quát về tử vi phương đông của 12 con giáp.",
-                            //   () {
-                            //     Get.to(() => const TuViTronDoiScreen());
-                            //   },
-                            // ),
-                            //TODO roy93~ impl
-                            // UIUtils.getButton(
-                            //   "Phong tục tập quán",
-                            //   Icons.favorite,
-                            //   description: "Phong tục tập quán - Phong tục ngày Tết Việt Nam",
-                            //   () {
-                            //     Get.to(() => const PhongTucTapQuanScreen());
-                            //   },
-                            // ),
-                            // UIUtils.getButton(
-                            //   "Phật pháp tổng hợp",
-                            //   Icons.family_restroom,
-                            //   description: "Cha mẹ, gia đình là điều quan trọng nhất trong cuộc đời mỗi con người",
-                            //   () {
-                            //     //TODO roy93~ ref https://ngaydep.com/phat-phap-ung-dung.html
-                            //     showSnackBarFull(StringConstants.warning, StringConstants.comingSoon);
-                            //   },
-                            // ),
-                            // UIUtils.getButton(
-                            //   "Blog cuộc sống",
-                            //   Icons.emoji_emotions,
-                            //   description:
-                            //       "Trong cuộc sống, chúng ta không thể tránh khỏi những phút giây mệt mỏi, áp lực hoặc những cảm xúc tiêu cực xảy đến mỗi ngày.",
-                            //   () {
-                            //     //TODO roy93~ ref https://ngaydep.com/blog-cuoc-song.html
-                            //     showSnackBarFull(StringConstants.warning, StringConstants.comingSoon);
-                            //   },
-                            // ),
-                            // UIUtils.getButton(
-                            //   "Phong thủy đời sống",
-                            //   Icons.warehouse_outlined,
-                            //   description:
-                            //       "Trưng bày cây cảnh trong phòng khách hay trong không gian ngôi nhà vốn là một thói quen tốt được nhiều người ưa chuộng.",
-                            //   () {
-                            //     //TODO roy93~ ref https://ngaydep.com/phong-thuy.html
-                            //     showSnackBarFull(StringConstants.warning, StringConstants.comingSoon);
-                            //   },
-                            // ),
-                            // UIUtils.getButton(
-                            //   "Nhân tướng học - Xem tướng, xem nốt ruồi, xem chỉ tay",
-                            //   Icons.monetization_on_rounded,
-                            //   description: "Trang thông tin ngày giờ và tử vi",
-                            //   () {
-                            //     //TODO roy93~ ref https://ngaydep.com/xem-tuong.html
-                            //     showSnackBarFull(StringConstants.warning, StringConstants.comingSoon);
-                            //   },
-                            // ),
-                            // UIUtils.getButton(
-                            //   "Bí mật về 12 cung hoàng đạo",
-                            //   Icons.ac_unit_sharp,
-                            //   description:
-                            //       "Mỗi cung hoàng đạo đều có tính cách khác nhau, có người dễ tính, thoải mái nhưng cũng có người luôn muốn được phục tùng, chiều chuộng",
-                            //   () {
-                            //     //TODO roy93~ ref https://ngaydep.com/bi-mat-12-cung-hoang-dao.html
-                            //     showSnackBarFull(StringConstants.warning, StringConstants.comingSoon);
-                            //   },
-                            // ),
                             UIUtils.getButton(
                               "Giới thiệu ứng dụng",
                               Icons.hotel_class,
@@ -523,7 +438,7 @@ class _ProfileScreenState extends BaseStatefulState<ProfileScreen> {
   Widget _buildBannerAd() {
     return Container(
       color: getBannerBackgroundColor(),
-      margin: const EdgeInsets.fromLTRB(0, 0, 0, 8),
+      margin: const EdgeInsets.fromLTRB(0, 0, 0, 4),
       child: MaxAdView(
         adUnitId: getBannerAdUnitId(),
         adFormat: AdFormat.banner,
